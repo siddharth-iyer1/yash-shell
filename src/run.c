226798f18497c0f74782b4f16742c3dee328bcbb
Siddharth <siddharth.iyer123@gmail.com>
2024-09-14 20:50:07 -0500

    fixed job printing, fg works now

--- src/run.c
+++ src/run.c
@@ -16,18 +16,20 @@ typedef struct {
     pid_t pid;
     char *command;
     job_status status;
+    int is_foreground;
 } job;
 
 job job_list[MAX_JOBS];
 int job_count = 0;
 int next_job_id = 1;
 
-void add_job(pid_t pid, char *command, job_status status) {
+void add_job(pid_t pid, char *command, job_status status, int is_foreground) {
     if (job_count < MAX_JOBS) {
         job_list[job_count].job_id = next_job_id++;
         job_list[job_count].pid = pid;
         job_list[job_count].command = strdup(command);
         job_list[job_count].status = status;
+        job_list[job_count].is_foreground = is_foreground;
         job_count++;
     } else {
         printf("Max job limit reached.\n");
@@ -51,6 +53,7 @@ void fg_command() {
     for (int i = job_count - 1; i >= 0; i--) {
         if (job_list[i].status == STOPPED || job_list[i].status == RUNNING) {
             job_list[i].status = RUNNING;
+            job_list[i].is_foreground = 1;
             printf("Bringing job [%d] to foreground: %s\n", job_list[i].job_id, job_list[i].command);
             kill(job_list[i].pid, SIGCONT);
 
@@ -66,9 +69,42 @@ void bg_command() {
     for (int i = job_count - 1; i >= 0; i--) {
         if (job_list[i].status == STOPPED) {
             job_list[i].status = RUNNING;
+            job_list[i].is_foreground = 0;
             printf("Bringing job [%d] to background: %s\n", job_list[i].job_id, job_list[i].command);
             kill(job_list[i].pid, SIGCONT);
             break;
+        }
+    }
+}
+
+void print_jobs() {
+    for (int i = 0; i < job_count; i++) {
+        char sign = '-';
+        if (i == job_count - 1 && job_list[i].is_foreground == 1){
+            sign = '+';
+        }
+
+        printf("[%d] %c %s       %s\n", job_list[i].job_id, sign,
+            job_list[i].status == RUNNING ? "Running" :
+            job_list[i].status == STOPPED ? "Stopped" : "Done",
+            job_list[i].command);
+    }
+}
+
+void check_background_jobs() {
+    int status;
+    pid_t pid;
+
+    for (int i = 0; i < job_count; i++) {
+        if (job_list[i].status == RUNNING) {
+            pid = waitpid(job_list[i].pid, &status, WNOHANG);
+            if (pid == job_list[i].pid) {
+                if (WIFEXITED(status) || WIFSIGNALED(status)) {
+                    job_list[i].status = DONE;
+                    printf("Job [%d] (%s) finished!\n", job_list[i].job_id, job_list[i].command);
+                    remove_job(pid);
+                }
+            }
         }
     }
 }
@@ -81,10 +117,21 @@ void execute_command(char **args) {
 
     int background = 0;
 
-    // Check if the last argument is "&"
+    // if to be sent to background
     if (i > 0 && strcmp(args[i-1], "&") == 0) {
         background = 1;
-        args[i-1] = NULL;  // Remove the "&" from the args array
+        args[i-1] = NULL;
+    }
+
+    if (strcmp(args[0], "fg") == 0) {
+        fg_command();
+        return;
+    } else if (strcmp(args[0], "bg") == 0) {
+        bg_command();
+        return;
+    } else if (strcmp(args[0], "jobs") == 0) {
+        print_jobs();
+        return;
     }
 
     pid_t pid = fork();
@@ -93,32 +140,22 @@ void execute_command(char **args) {
         perror("Fork failed");
         return;
 
-    } else if (pid == 0) {  // Child process
-        if (strcmp(args[0], "fg") == 0) {
-            fg_command();
-        } else if (strcmp(args[0], "bg") == 0) {
-            bg_command();
-        } else if (strcmp(args[0], "jobs") == 0) {
-            printf("jobs\n");
-            printf("jobs\n");
-            printf("jobs\n");
-            printf("jobs\n");
-            printf("jobs\n");
-        } else {
-            // Execute the command
-            if (execvp(args[0], args) < 0) {
-                perror("Execution failed");
-                exit(1);
-            }
+    } else if (pid == 0) {  // child
+        if (execvp(args[0], args) < 0) {
+            perror("Execution failed");
+            exit(1);
         }
-    } else {  // Parent process
+        exit(0);
+
+    } else {  // parent
         if (background) {
             printf("Running command in the background, PID: %d\n", pid);
-            add_job(pid, args[0], RUNNING);
+            add_job(pid, args[0], RUNNING, 0);
         } else {
-            // Wait for the child process to finish
             int status;
             waitpid(pid, &status, 0);
         }
+
+        check_background_jobs();
     }
 }
